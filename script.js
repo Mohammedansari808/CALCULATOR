53138047bf0a597eead2fd7b505c18cfa6df8708
Mohammed Ansari <112924825+Mohammedansari808@users.noreply.github.com>
2022-10-17 13:13:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+let div = document.createElement("div")
+div.setAttribute("class","di")
+
+let titl = document.createElement("h1")
+titl.setAttribute("id","title")
+titl.innerHTML="CALCULATOR";
+titl.style.fontFamily="'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif"
+titl.style.position ="absolute"
+
+
+let para = document.createElement("p")
+para.setAttribute("id","description")
+para.innerHTML="A Normal Calculator"
+para.style.position ="absolute"
+para.style.marginTop ="35px"
+
 let divs = document.createElement("div")
 divs.setAttribute("class","calculator")
 divs.style.height="300px"
@@ -6,16 +22,14 @@ divs.style.paddingLeft="10px"
 divs.style.boxShadow="4px 4px 30px rgba(0,0,0,0.3)"
 divs.style.backgroundColor="aqua"
 divs.style.display="flex"
+divs.style.alignContent='center'
+divs.style.marginTop="200px"
 divs.style.borderRadius="10px"
-
 
 
 let table = document.createElement("table")
 table.setAttribute("class","table")
 table.style.display="grid";
-
-
-
 
 
 function row(tagname){
@@ -41,7 +55,7 @@ tdisplay.style.justifyContent="center"
 
 
 let input = document.createElement("input");
-input.setAttribute("id","ll")
+input.setAttribute("id","result")
 input.style.textAlign="right"
 input.style.fontSize="large"
 input.style.borderRadius="4px"
@@ -67,33 +81,45 @@ let row1 = row("tr");
 let row2 = row("tr");
 let td1 = data("td","scope","col")
 let btn1 = button("button",'onclick',"send(1)",1)
+btn1.setAttribute("id",1)
 let td2 = data("td","scope","col")
 let btn2 = button("button",'onclick',"send(2)",2)
+btn2.setAttribute("id",2)
 let td3 = data("td","scope","col")
 let btn3 = button("button",'onclick',"send(3)",3)
+btn3.setAttribute("id",3)
 let td4 = data("td","scope","col")
 let btn4 = button("button",'onclick',"send(4)",4)
+btn4.setAttribute("id",4)
 let td5 = data("td","scope","col")
 let btn5 = button("button",'onclick',"send(5)",5)
+btn5.setAttribute("id",5)
 
 let row3=row("tr")
 let td6 =data("td","scope","col")
 let bt6 = button("button",'onclick',"send(6)",6)
+bt6.setAttribute("id",6)
 let td7 = data("td","scope","col")
 let btn7 = button("button",'onclick',"send(7)",7)
+btn7.setAttribute("id",7)
 let td8 = data("td","scope","col")
 let btn8 = button("button",'onclick',"send(8)",8)
+btn8.setAttribute("id",8)
 let td9 = data("td","scope","col")
 let btn9 = button("button",'onclick',"send(9)",9)
+btn9.setAttribute("id",9)
 let td10 = data("td","scope","col")
 let btn0 = button("button",'onclick',"send(0)",0)
+btn0.setAttribute("id",0)
 
 let row4=row("tr")
 
 let td11 = data("td","scope","col")
 let btAdd= button("button",'onclick',"send('+')","+")
+btAdd.setAttribute("id","add")
 let td12 = data("td","scope","col")
 let btnS = button("button",'onclick',"send('-')","-")
+btnS.setAttribute("id","subtract")
 let td13 = data("td","scope","col")
 let btnM = button("button",'onclick',"send('*')","*")
 let td14 = data("td","scope","col")
@@ -103,6 +129,7 @@ let td15 = data("td","scope","col")
 let btnE = document.createElement("button");
 btnE.setAttribute("onclick","calculate()")
 btnE.setAttribute('class','equal')
+btnE.setAttribute("id","equal")
 btnE.innerHTML ="="
 btnE.style.height="40px"
 btnE.style.width="40px"
@@ -117,6 +144,9 @@ let td16 = data("td","scope","col")
 let btnDOt = button("button",'onclick',"send('.')",".")
 let td17 = data("td","scope","col")
 let btnClear = button("button",'onclick',"Clear()","AC")
+btnClear.setAttribute("id","clear")
+
+
 let td18 =data("td","scope","col")
 let btnRoot = button("button",'onclick',"Root()","√")
 let td19 = data('td',"scope","col")
@@ -152,21 +182,29 @@ row4.append(td15,td9,td10,td11,td12)
 row5.append(td13,td14,td16,td18,td19,)
 
 table.append(row1,row2,row3,row4,row5)
+
 divs.append(table)
+div.append(divs)
 
-document.body.append(divs)
+document.body.append(divs,titl,para)
 
-let x= document.getElementById("ll")
+let x= document.getElementById("result")
+
 
 function send (num){
     x.value += num
 }
+
+
+
 function calculate(){
 try{
     x.value= eval(x.value)
 }catch(err){
     alert("Only numbers are allowed")
+
 }
+
 }
 function del(){
     x.value = x.value.slice(0,-1)
